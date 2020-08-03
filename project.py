fb7780a050d09cd70908858e6c8e2e427ca8652d
Tyrell Harris <jerry.th37@gmail.com>
2020-08-03 00:45:45 -0400

    Added title and introductions

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, make_response
 app = Flask(__name__, static_url_path='/index', static_folder='')
 
 
+
 @app.route('/')
 @app.route('/index')
 def index():
